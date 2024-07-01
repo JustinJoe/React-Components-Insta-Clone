f02aa7d7e5edd691983fd2321a281342a11c56bd
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-07-01 01:40:28 +0000

    Completes MVP

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,9 +5,11 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  const renderedComments = comments.map(comment => <Comment key={comment.id} comment={comment} />)
 
   return (
     <div>
+      {renderedComments}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
